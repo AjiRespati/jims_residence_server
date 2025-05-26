948af9e5e64c23e4333c7253b4e4c1679c227c89
Aji Respati <aji@voltras.co.id>
2025-05-26 13:19:01 +0700

    1.0.7+2 perbaikan status n sorting

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const { startBillingTask } = require('./tasks/billingTasks'); // Adjust the path
 const app = express();
 
 //TODO: VERSIONING
-const version = "1.0.7+1";
+const version = "1.0.7+2";
 
 // ✅ Middlewares
 app.use(cors());
