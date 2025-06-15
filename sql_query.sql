4e5f60c37720020d748a05098fd435b34add214a
Aji Respati <aji@voltras.co.id>
2025-06-15 07:06:42 +0700

    saving query

---
+++ sql_query.sql
@@ -0,0 +1,23 @@
+
+
+
+---------------------
+-- FIX EMAIL SALES --
+---------------------
+
+
+UPDATE "public"."Invoices"
+SET "dueDate" = '2025-06-18T00:00:00+07:00'
+WHERE id = 'f4fd5656-a9a0-455a-8fc6-547edae73c06';
+
+
+UPDATE "public"."Invoices"
+SET "dueDate" = '2025-06-14T00:00:00+07:00'
+WHERE id = '09fcdc9c-e08d-400d-9805-065409202969';
+
+
+UPDATE "public"."Invoices"
+SET "dueDate" = '2025-06-15T00:00:00+07:00'
+WHERE id = 'edb9f054-d1b8-439d-bf2b-742080689090';
+
+
