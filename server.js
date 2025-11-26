d4fee2bd71653731b70c1817f0a73d9a5480081f
Aji Respati <aji@voltras.co.id>
2025-11-26 08:18:15 +0700

    1.2.3 transfer owner done

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const { startBillingTask } = require('./tasks/billingTasks'); // Adjust the path
 const app = express();
 
 //TODO: VERSIONING
-const version = "1.2.2";
+const version = "1.2.3";
 
 // ✅ Middlewares
 app.use(cors());
