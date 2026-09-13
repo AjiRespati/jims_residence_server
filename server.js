1c49322e63b939183fbb87d6b795cd63fc5e0901
Aji Respati <aji@voltras.co.id>
2026-09-13 16:58:39 +0700

    1.2.6 up version

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const { startBillingTask } = require('./tasks/billingTasks'); // Adjust the path
 const app = express();
 
 //TODO: VERSIONING
-const version = "1.2.5";
+const version = "1.2.6";
 
 // ✅ Middlewares
 app.use(cors());
